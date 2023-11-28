b1cb7232113fea13f98c74dbd89e54cebabfdf5d
Ye Tian <eddietian06@gmail.com>
2023-11-28 15:18:58 -0500

    css for name card; define variables for the chat screen in JS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,23 @@
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+// definfe variables for the chat
+let aniProfile1 = document.querySelector("aniProfile1");
+let greeting = document.querySelector(".hello");
+let questionWhere = document.querySelector(".where")
+let aniProfile2 = document.querySelector("aniProfile2");
+let answerWhere = document.querySelector(".answerWhere");
+let questionFood = document.querySelector(".questionFood");
+let aniProfile3 = document.querySelector("aniProfile3");
+let answerFood = document.querySelector(".answerFood");
+let questionSound = document.querySelector(".questionSound");
+let aniProfile4 = document.querySelector("aniProfile4");
+let answerSound = document.querySelector(".answerSound");
+
+// define variables for the prompts
+let prompt1 = document.querySelector(".promptWhere");
+let prompt2 = document.querySelector(".promptFood");
+let prompt3 = document.querySelector(".promptSound");
+
+// define functions
