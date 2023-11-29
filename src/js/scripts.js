f768a5e27a965901aec1354d736a71b0bdb8e2ce
Ye Tian <eddietian06@gmail.com>
2023-11-28 21:00:16 -0500

    work in 1128 Lecture: more html and css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,21 @@
 // - You'll need to link this file to your HTML :)
 
 // definfe variables for the chat
-let aniProfile1 = document.querySelector("aniProfile1");
+let aniProfile1 = document.querySelector("ani-profile1");
 let greeting = document.querySelector(".hello");
 let questionWhere = document.querySelector(".where")
-let aniProfile2 = document.querySelector("aniProfile2");
-let answerWhere = document.querySelector(".answerWhere");
-let questionFood = document.querySelector(".questionFood");
-let aniProfile3 = document.querySelector("aniProfile3");
-let answerFood = document.querySelector(".answerFood");
-let questionSound = document.querySelector(".questionSound");
-let aniProfile4 = document.querySelector("aniProfile4");
-let answerSound = document.querySelector(".answerSound");
+let aniProfile2 = document.querySelector("ani-profile2");
+let answerWhere = document.querySelector(".answer-where");
+let questionFood = document.querySelector(".question-food");
+let aniProfile3 = document.querySelector("ani-profile3");
+let answerFood = document.querySelector(".answer-food");
+let questionSound = document.querySelector(".question-sound");
+let aniProfile4 = document.querySelector("ani-profile4");
+let answerSound = document.querySelector(".answer-sound");
 
 // define variables for the prompts
-let prompt1 = document.querySelector(".promptWhere");
-let prompt2 = document.querySelector(".promptFood");
-let prompt3 = document.querySelector(".promptSound");
+let prompt1 = document.querySelector(".prompt-where");
+let prompt2 = document.querySelector(".prompt-food");
+let prompt3 = document.querySelector(".prompt-sound");
 
 // define functions
